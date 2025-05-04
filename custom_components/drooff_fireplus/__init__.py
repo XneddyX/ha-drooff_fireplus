996166036e6b47240ac2256ebc86b3fd7f119e08
XneddyX <neddy@gmx.net>
2025-05-04 20:51:47 +0200

    Fix logger initialization in DrooffDataUpdateCoordinator setup

--- custom_components/drooff_fireplus/__init__.py
+++ custom_components/drooff_fireplus/__init__.py
@@ -1,11 +1,11 @@
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 
-from .const import DOMAIN
+from .const import DOMAIN, LOGGER
 from .coordinator import DrooffDataUpdateCoordinator
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    coordinator = DrooffDataUpdateCoordinator(hass, entry)
+    coordinator = DrooffDataUpdateCoordinator(hass, entry logger=LOGGER,)
     await coordinator.async_config_entry_first_refresh()
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
     hass.async_create_task(
