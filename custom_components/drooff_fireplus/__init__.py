bde22e86a0556bec0cd6340533ff8623ffc6cf78
XneddyX <neddy@gmx.net>
2025-05-04 20:52:22 +0200

    Fix syntax error in DrooffDataUpdateCoordinator initialization

--- custom_components/drooff_fireplus/__init__.py
+++ custom_components/drooff_fireplus/__init__.py
@@ -5,7 +5,7 @@ from .const import DOMAIN, LOGGER
 from .coordinator import DrooffDataUpdateCoordinator
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry):
-    coordinator = DrooffDataUpdateCoordinator(hass, entry logger=LOGGER,)
+    coordinator = DrooffDataUpdateCoordinator(hass, entry, logger=LOGGER)
     await coordinator.async_config_entry_first_refresh()
     hass.data.setdefault(DOMAIN, {})[entry.entry_id] = coordinator
     hass.async_create_task(
